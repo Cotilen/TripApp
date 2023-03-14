fc57f20a70b40d0ffe19a7a6df5eae27f6411e85
Cleiton <cleitonncruz105@gmail.com>
2023-03-14 11:12:16 -0300

    Sign In Completa

--- app/src/main/java/br/senai/sp/jandira/tripapp/SignUpActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/tripapp/SignUpActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
@@ -43,6 +44,7 @@ fun SignUpScreen() {
 
     Surface(modifier = Modifier.fillMaxSize()) {
 
+        val context = LocalContext.current
         var checked by remember {
             mutableStateOf(false)
         }
@@ -84,149 +86,155 @@ fun SignUpScreen() {
                     color = Color(160, 156, 156)
                 )
 
-                Column(modifier = Modifier.padding(17.dp)) {
-                    OutlinedTextField(value = "",
-                        onValueChange = {""
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        shape = RoundedCornerShape(16.dp),
-                        label = { Text(stringResource(id = R.string.username)) },
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.person_24),
-                                contentDescription = "Image_email",
-                                modifier = Modifier
-                                    .height(27.dp)
-                                    .width(22.dp),
-                                tint = Color(207, 6, 240)
-                            )
-                        })
 
 
-                    OutlinedTextField(value = "",
-                        onValueChange = {""
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-
-                        shape = RoundedCornerShape(16.dp),
-                        label = { Text(stringResource(id = R.string.phone)) },
-                        visualTransformation = PasswordVisualTransformation(),
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.phone_24),
-                                contentDescription = "Image_password",
-                                modifier = Modifier
-                                    .height(27.dp)
-                                    .width(22.dp),
-                                tint = Color(207, 6, 240)
-                            )
-                        })
-
-
-                    OutlinedTextField(value = "",
-                        onValueChange = {""
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        shape = RoundedCornerShape(16.dp),
-                        label = { Text(stringResource(id = R.string.email)) },
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.email),
-                                contentDescription = "Image_email",
-                                modifier = Modifier
-                                    .height(27.dp)
-                                    .width(22.dp),
-                                tint = Color(207, 6, 240)
-                            )
-                        })
-
-
-                    OutlinedTextField(value = "",
-                        onValueChange = {""
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-
-                        shape = RoundedCornerShape(16.dp),
-                        label = { Text(stringResource(id = R.string.password)) },
-                        visualTransformation = PasswordVisualTransformation(),
-                        leadingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.password),
-                                contentDescription = "Image_password",
-                                modifier = Modifier
-                                    .height(27.dp)
-                                    .width(22.dp),
-                                tint = Color(207, 6, 240)
-                            )
-                        })
-
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Checkbox(
-                            checked = checked,
-                            onCheckedChange = {checked = it},
-                            modifier = Modifier
-                                .height(27.dp)
-                                .width(27.dp)
-
-                        )
-                        Text(text = stringResource(id = R.string.over_18),
-                            fontSize = 18.sp,
-                            lineHeight = 21.sp)
-                    }
-
-                }
-
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(end = 17.dp),
-                    horizontalAlignment = Alignment.End) {
-                    Button(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier
-                            .width(327.dp)
-                            .height(48.dp),
-                        shape = RoundedCornerShape(16.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color(207, 6, 240)
-                        )
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.create).uppercase(),
-                            color = Color.White
-                        )
-                        Icon(painter = painterResource(id = R.drawable.arrow_forward_24),
-                            contentDescription ="",
-                            tint = Color(255,255,255)
-                        )
-
-                    }
-                    Spacer(modifier = Modifier.height(31.dp))
-
-                    Row() {
-                        Text(
-                            text = stringResource(id = R.string.already_account),
-                            color = Color(160, 156, 156),
-                            fontSize = 12.sp,
-                            fontWeight = FontWeight(400),
-                            lineHeight = 18.sp
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-
-                        ClickableText(text = AnnotatedString(text = stringResource(id = R.string.sign_in)
-                        ), style = TextStyle(
-                            color = Color(207, 6, 240),
-                            fontSize = 12.sp,
-                            fontWeight = FontWeight(700),
-                            lineHeight = 18.sp,
-                        ) , onClick ={}
-                        )
-
-
-                    }
             }
 
+            Column(modifier = Modifier.padding(start = 17.dp, top = 50.dp, end = 17.dp)) {
+                OutlinedTextField(value = "",
+                    onValueChange = {""
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    label = { Text(stringResource(id = R.string.username)) },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.person_24),
+                            contentDescription = "Image_email",
+                            modifier = Modifier
+                                .height(27.dp)
+                                .width(22.dp),
+                            tint = Color(207, 6, 240)
+                        )
+                    })
+
+
+                OutlinedTextField(value = "",
+                    onValueChange = {""
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+
+                    shape = RoundedCornerShape(16.dp),
+                    label = { Text(stringResource(id = R.string.phone)) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.phone_24),
+                            contentDescription = "Image_password",
+                            modifier = Modifier
+                                .height(27.dp)
+                                .width(22.dp),
+                            tint = Color(207, 6, 240)
+                        )
+                    })
+
+
+                OutlinedTextField(value = "",
+                    onValueChange = {""
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    label = { Text(stringResource(id = R.string.email)) },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.email),
+                            contentDescription = "Image_email",
+                            modifier = Modifier
+                                .height(27.dp)
+                                .width(22.dp),
+                            tint = Color(207, 6, 240)
+                        )
+                    })
+
+
+                OutlinedTextField(value = "",
+                    onValueChange = {""
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+
+                    shape = RoundedCornerShape(16.dp),
+                    label = { Text(stringResource(id = R.string.password)) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.password),
+                            contentDescription = "Image_password",
+                            modifier = Modifier
+                                .height(27.dp)
+                                .width(22.dp),
+                            tint = Color(207, 6, 240)
+                        )
+                    })
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = {checked = it},
+                        modifier = Modifier
+                            .height(27.dp)
+                            .width(27.dp)
+
+                    )
+                    Text(text = stringResource(id = R.string.over_18),
+                        fontSize = 18.sp,
+                        lineHeight = 21.sp)
+                }
+
+            }
+
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 17.dp),
+                horizontalAlignment = Alignment.End) {
+                Spacer(modifier = Modifier.height(31.dp))
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .width(327.dp)
+                        .height(48.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(207, 6, 240)
+                    )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.create).uppercase(),
+                        color = Color.White
+                    )
+                    Icon(painter = painterResource(id = R.drawable.arrow_forward_24),
+                        contentDescription ="",
+                        tint = Color(255,255,255)
+                    )
+
+                }
+                Spacer(modifier = Modifier.height(31.dp))
+
+                Row() {
+                    Text(
+                        text = stringResource(id = R.string.already_account),
+                        color = Color(160, 156, 156),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight(400),
+                        lineHeight = 18.sp
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    ClickableText(text = AnnotatedString(text = stringResource(id = R.string.sign_in)
+                    ), style = TextStyle(
+                        color = Color(207, 6, 240),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight(700),
+                        lineHeight = 18.sp,
+                    ) , onClick ={
+                        val openSignIn = Intent(context,MainActivity::class.java)
+                        context.startActivity(openSignIn)}
+                    )
+
+
+                }
             }
             Column(
                 modifier = Modifier.fillMaxSize(),
