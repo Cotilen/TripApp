6209efdb6dc1d12475b4774ed85172768fa0ea10
Cleiton <cleitonncruz105@gmail.com>
2023-04-04 11:28:15 -0300

    Update SignUpActivity.kt

--- app/src/main/java/br/senai/sp/jandira/tripapp/gui/SignUpActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/tripapp/gui/SignUpActivity.kt
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.tripapp.gui
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -33,11 +34,33 @@ import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.tripapp.R
 import br.senai.sp.jandira.tripapp.components.BottomShape
 import br.senai.sp.jandira.tripapp.components.TopShape
+import br.senai.sp.jandira.tripapp.dao.repository.UserRepository
+import br.senai.sp.jandira.tripapp.model.User
 import br.senai.sp.jandira.tripapp.ui.theme.TripAppTheme
 
 class SignUp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        val user = User(
+            userName = "Maria da Silva",
+            email = "MariaSilva@gmail.com",
+            password = "123456",
+            phone = "(11)95161-23672",
+            isOver18 = true
+        )
+
+        val userRep = UserRepository(this)
+
+        var id = userRep.save(user)
+
+        Toast.makeText(
+            this,
+            "$id",
+            Toast.LENGTH_LONG
+        ).show()
+        
         setContent {
             TripAppTheme {
                 SignUpScreen()
@@ -72,9 +95,11 @@ fun SignUpScreen() {
             ) {
                 TopShape()
             }
-            Column(modifier = Modifier
-                .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
 
                 Text(
@@ -94,27 +119,39 @@ fun SignUpScreen() {
                 Spacer(modifier = Modifier.height(32.dp))
 
                 Box(modifier = Modifier.size(100.dp)) {
-                    Card(modifier = Modifier
-                        .size(100.dp),
-                    shape = CircleShape,
-                    backgroundColor = Color(232,232,232,255)) {
-                        Image(painter = painterResource(id = R.drawable.user), contentDescription =null
-                       )
-                        
+                    Card(
+                        modifier = Modifier
+                            .size(100.dp),
+                        shape = CircleShape,
+                        backgroundColor = Color(232, 232, 232, 255)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.user),
+                            contentDescription = null
+                        )
+
                     }
-                    
-                    Image(painter = painterResource(id = R.drawable.photo_24), contentDescription =null,
-                    modifier = Modifier.align(Alignment.BottomEnd))
+
+                    Image(
+                        painter = painterResource(id = R.drawable.photo_24),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
                 }
 
 
             }
 
-            Column(modifier = Modifier.height(460.dp)
-                .padding(start = 17.dp, top = 30.dp, end = 17.dp)
-                .verticalScroll(rememberScrollState())) {
-                OutlinedTextField(value = "",
-                    onValueChange = {""
+            Column(
+                modifier = Modifier
+                    .height(460.dp)
+                    .padding(start = 17.dp, top = 30.dp, end = 17.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {
+                        ""
                     },
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp),
@@ -131,11 +168,15 @@ fun SignUpScreen() {
                     },
                     colors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color(207, 6, 240),
-                        unfocusedBorderColor = Color(207, 6, 240)))
+                        unfocusedBorderColor = Color(207, 6, 240)
+                    )
+                )
 
 
-                OutlinedTextField(value = "",
-                    onValueChange = {""
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {
+                        ""
                     },
                     modifier = Modifier.fillMaxWidth(),
 
@@ -151,13 +192,17 @@ fun SignUpScreen() {
                                 .width(22.dp),
                             tint = Color(207, 6, 240)
                         )
-                    },colors = TextFieldDefaults.outlinedTextFieldColors(
+                    }, colors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color(207, 6, 240),
-                        unfocusedBorderColor = Color(207, 6, 240)))
+                        unfocusedBorderColor = Color(207, 6, 240)
+                    )
+                )
 
 
-                OutlinedTextField(value = "",
-                    onValueChange = {""
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {
+                        ""
                     },
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp),
@@ -171,13 +216,17 @@ fun SignUpScreen() {
                                 .width(22.dp),
                             tint = Color(207, 6, 240)
                         )
-                    },colors = TextFieldDefaults.outlinedTextFieldColors(
+                    }, colors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color(207, 6, 240),
-                        unfocusedBorderColor = Color(207, 6, 240)))
+                        unfocusedBorderColor = Color(207, 6, 240)
+                    )
+                )
 
 
-                OutlinedTextField(value = "",
-                    onValueChange = {""
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {
+                        ""
                     },
                     modifier = Modifier.fillMaxWidth(),
 
@@ -196,29 +245,37 @@ fun SignUpScreen() {
                     },
                     colors = TextFieldDefaults.outlinedTextFieldColors(
                         focusedBorderColor = Color(207, 6, 240),
-                        unfocusedBorderColor = Color(207, 6, 240)))
+                        unfocusedBorderColor = Color(207, 6, 240)
+                    )
+                )
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Row(modifier = Modifier.padding(start = 17.dp),
-                    verticalAlignment = Alignment.CenterVertically) {
+                Row(
+                    modifier = Modifier.padding(start = 17.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Checkbox(
                         checked = checked,
-                        onCheckedChange = {checked = it},
+                        onCheckedChange = { checked = it },
                         modifier = Modifier
                             .height(27.dp)
                             .width(27.dp)
 
                     )
-                    Text(text = stringResource(id = R.string.over_18),
+                    Text(
+                        text = stringResource(id = R.string.over_18),
                         fontSize = 18.sp,
-                        lineHeight = 21.sp)
+                        lineHeight = 21.sp
+                    )
                 }
 
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(end = 17.dp),
-                    horizontalAlignment = Alignment.End) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 17.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
                     Spacer(modifier = Modifier.height(31.dp))
 
                     Button(
@@ -235,9 +292,10 @@ fun SignUpScreen() {
                             text = stringResource(id = R.string.create).uppercase(),
                             color = Color.White
                         )
-                        Icon(painter = painterResource(id = R.drawable.arrow_forward_24),
-                            contentDescription ="",
-                            tint = Color(255,255,255)
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrow_forward_24),
+                            contentDescription = "",
+                            tint = Color(255, 255, 255)
                         )
 
                     }
@@ -253,15 +311,17 @@ fun SignUpScreen() {
                         )
                         Spacer(modifier = Modifier.width(4.dp))
 
-                        ClickableText(text = AnnotatedString(text = stringResource(id = R.string.sign_in)
+                        ClickableText(text = AnnotatedString(
+                            text = stringResource(id = R.string.sign_in)
                         ), style = TextStyle(
                             color = Color(207, 6, 240),
                             fontSize = 12.sp,
                             fontWeight = FontWeight(700),
                             lineHeight = 18.sp,
-                        ) , onClick ={
-                            val openSignIn = Intent(context,MainActivity::class.java)
-                            context.startActivity(openSignIn)}
+                        ), onClick = {
+                            val openSignIn = Intent(context, MainActivity::class.java)
+                            context.startActivity(openSignIn)
+                        }
                         )
 
 
@@ -277,5 +337,6 @@ fun SignUpScreen() {
                 BottomShape()
             }
 
+        }
     }
-}}
+}
